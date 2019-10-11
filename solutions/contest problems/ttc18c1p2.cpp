de43098df4ed4f34cbc2291eb20db255c9a97d7f
Kevin Wang <56416268+kevin-j-wang@users.noreply.github.com>
2019-10-11 10:37:00 -0400

    Update ttc18c1p2.cpp

--- solutions/contest problems/ttc18c1p2.cpp
+++ solutions/contest problems/ttc18c1p2.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 int main() {
-  int n;
-  cin >> n;
-  string stuff [n];
-  int other [n];
+  int n; //number of elements
+  cin >> n; 
+  string stuff [n]; //string array with number of elements equal to number of elements which is equal to the string input
+  int other [n];    //int array with the same number of elements which is equal to the int inputs
   for(int i = 0; i < n; i++) {
-    cin >> stuff [i];
-    cin >> other [i];
+    cin >> stuff [i]; //assigns the inputs to the array
+    cin >> other [i]; //same thing
   }
-  for(int i = 0; i < n; i++){
-    if(other[0] <= other[i]) {
+  for(int i = 0; i < n; i++){ //does it "number of elements" times
+    if(other[0] <= other[i]) { //if the number is less than or equal to the other number, swap the next 2 elements
       other[0] = other[i];
       stuff[0] = stuff[i];
     }
   }
-  cout << stuff [0];
+  cout << stuff [0]; //output
 }
