22d2e67acf6a5c60fb10303f34c9b06a0c87c302
firetto <azs.zavyalov@gmail.com>
2019-10-11 18:33:07 -0400

    Created helloworld.cpp
    Hello World in C++

---
+++ solutions/uncategorized/helloworld.cpp
@@ -0,0 +1,15 @@
+// includes the input/output library, that allows you to input/output using std::cout and std::cin
+#include <iostream>
+
+// allows you to use std:: namespace functions without typing "std::" before each one
+using namespace std;
+
+// main function, executed when code is run
+int main() {
+    
+    // outputs Hello, World!
+    cout << "Hello, World!";
+    
+    // since the main function is an int, a value can be returned (actually not required in C99 or C++, and can be omitted)
+    return 0;
+}
