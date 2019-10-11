f6ba37018a5cdcfe53ae8326a3fed2997e05cb2e
HARRIBO4242 <56176564+HARRIBO4242@users.noreply.github.com>
2019-10-10 23:25:03 -0400

    Create DigitsGame.py

---
+++ DigitsGame.py
@@ -0,0 +1,8 @@
+number = input()
+#get each individual digit from the number
+digit_1 = int(number[0])
+digit_2 = int(number[1])
+digit_3 = int(number[2])
+digit_4 = int(number[3])
+#print the sum of all the individual digits
+print(digit_1+digit_2+digit_3+digit_4)
