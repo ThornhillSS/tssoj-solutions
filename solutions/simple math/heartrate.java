08e65a32a4aa193a7fa9be67eea393d1d9601152
stefpi <ceatanostru@gmail.com>
2020-10-27 16:42:36 -0400

    create heartrate.java
    a java solution to the tssoj problem "Heart Rate" (https://tssoj.ca/problem/heartrate)

---
+++ solutions/simple math/heartrate.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class ladyofthelake {
+	public static void main(String[] args) throws Exception {
+	    Scanner input = new Scanner(System.in);
+	    
+	    int age = input.nextInt();
+	    int restingHeart = input.nextInt();
+	    double intensity = input.nextDouble();
+	    
+	    int maxHeart = 220 - age;
+	    int heartReserve = maxHeart - restingHeart;
+	    
+	    System.out.print((double)restingHeart + (double)(intensity*heartReserve));
+	}
+}
