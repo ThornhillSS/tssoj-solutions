3af191765c847c677eb8fcfcc1133eec17db74cf
encodeous <encodeous7@gmail.com>
2020-10-27 17:58:14 -0400

    Change class name

--- solutions/simple math/heartrate.java
+++ solutions/simple math/heartrate.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class ladyofthelake {
+public class Main {
 	public static void main(String[] args) throws Exception {
 	    Scanner input = new Scanner(System.in);
 	    
