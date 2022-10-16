4e505c3d8ea4acf8edfb52deddb4d1848d3b8ad0
Am1r__8 | amirsol.eth <68626539+Am1r8@users.noreply.github.com>
2022-10-16 18:19:20 -0400

    ccc 2022 J2 Solution

--- solutions/contest problems/ccc22j2.py
+++ solutions/contest problems/ccc22j2.py
@@ -1,3 +1,11 @@
+# Fergusonball players are given a star rating based on the number of points that they score and the number of fouls that they commit.
+# Specifically, they are awarded 5 stars for each point scored, and 3 stars are taken away for each foul committed. For every player,
+# the number of points that they score is greater than the number of fouls that they commit.
+# Your job is to determine how many players on a team have a star rating greater than 40.
+# You also need to determine if the team is considered a gold team which means that all the players have a star rating greater than 40.
+# Created by Amirhosein Soleimanian
+# LINK: https://tssoj.ca/problem/fergusonballratings
+
 num = int(input())
 li = []
 count = 0
