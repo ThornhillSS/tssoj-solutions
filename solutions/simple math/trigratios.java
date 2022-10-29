0999a7e645386ed79ad008f09ac1bc4939c5ed0c
Kyle <70961538+zzadxz@users.noreply.github.com>
2022-10-28 20:00:04 -0400

    Add files via upload

---
+++ solutions/simple math/trigratios.java
@@ -0,0 +1,16 @@
+package Chapter5;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.lang.Math;
+public class trigratios {
+    public static void main(String[] args) {
+        DecimalFormat formatCommas = new DecimalFormat("#.##");
+        Scanner myscanner = new Scanner(System.in);
+        int angle = myscanner.nextInt();
+        double angler = Math.toRadians(angle); // converts the angle in degree to radians
+        System.out.println(formatCommas.format(Math.sin(angler)));
+        System.out.println(formatCommas.format(Math.cos(angler)));
+        System.out.println(formatCommas.format(Math.tan(angler)));
+
+    }
+}
